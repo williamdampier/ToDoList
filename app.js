0bfe4d3294eb4345f9d281fa45b11a35f67c5489
Vladi Kuban <kuban.vladimir88@gmail.com>
2021-02-05 14:35:49 +1300

    Added comments

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 //jshint esversion:6
-const express = require("express"); //adding express module
-const bodyParser = require("body-parser"); //adding HTML Body Parser module
-const _ = require('lodash');
 
-const app = express(); //defining app to use express
+/* Adding modules required: express, mongoose(for MongoDB connection), body parser, lodash(for custom link list Capitalize function)*/
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const _ = require('lodash');
 const mongoose = require('mongoose');
 
+const app = express(); //defining app to use express
 
-app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+app.set('view engine', 'ejs'); //enable ejs to be used
 
+app.use(bodyParser.urlencoded({extended: true})); //enable body-parser to be used
+app.use(express.static('public')); //enable publuc folder via express to access css
+
+
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }); //connect to DB
+/*-------------------------------------------------------------------------------------*/
+
+/* Default Items list Schema init*/
 const itemsSchema = {
   name: String
 }
 
-const Item = mongoose.model("Item", itemsSchema);
+/*default items model and list creation */
+const Item = mongoose.model("Item", itemsSchema); //Schema
 
+//Items
 const item1 = new Item({
   name: "First Item"
 });
@@ -32,8 +42,12 @@ const item3 = new Item({
   name: "Third Item"
 });
 
-const defaultItems = [item1, item2, item3];
+const defaultItems = [item1, item2, item3]; //array of default items will be passed into List
 
+/*-------------------------------------------------------------------------------------*/
+
+
+// This List Schema and Model will be used to create custom lists
 const listSchema = {
   name: String,
   items: [itemsSchema]
@@ -42,10 +56,13 @@ const listSchema = {
 
 const List = new mongoose.model("List", listSchema);
 
+/*-------------------------------------------------------------------------------------*/
 
+/* Main app section */
 
 app.get("/", function(req,res)
 {
+  // If no Lists exist -> create first list passing default items in
   Item.find({}, function (err, foundItems){
     if (foundItems.length === 0) {
       Item.insertMany(defaultItems, function(err){
@@ -53,6 +70,7 @@ app.get("/", function(req,res)
       });
     }
     else {
+    //if existing list found or passed on from redirect(<List Name>) -> render page & show the list
       res.render("list", {listTitle:"Today", newListItems:foundItems});
     }
   });
@@ -62,52 +80,58 @@ app.get("/", function(req,res)
 
 });
 
+// /Post method handles new items added to the list function
 app.post("/", function(req,res)
   {
-    const itemName = req.body.newItem;
-    const listName = req.body.list;
+    const itemName = req.body.newItem; //Item name taken from the input
+    const listName = req.body.list; //List name taken from Page <%= listTitle %>
 
+    // create Item that will be added to the List
     const item = new Item({
       name : itemName
     });
-
+//If List name <%= listTitle %> is default, which is called "Today", add created item to that list then save in DB
 if (listName === "Today") {
 
     item.save();
     res.redirect("/");
   } else {
+    //Otherwise, take List name from a title and add new item to that list
     List.findOne({name: listName}, function(err, foundList){
       foundList.items.push(item);
       foundList.save();
-      res.redirect("/" + listName);
+      res.redirect("/" + listName); //redirect to root -> for render function
     });
   }
   });
 
+/* Checkbox will delete items from the list, handled by processing name="checkbox" value="<%=item._id% inside <checkbox> input*/
 app.post("/delete", function(req, res){
 
-  const checkedItemId = req.body.checkbox;
-  const listName = req.body.listName;
-if (listName === "Today")
+  const checkedItemId = req.body.checkbox; //ItemId this checkbox belongs to
+  const listName = req.body.listName; //listName from which we delete the item - from <%= listTitle %>
+
+if (listName === "Today") //check if we are working with default list
 {
-  Item.findByIdAndRemove(checkedItemId, function(err)
+  Item.findByIdAndRemove(checkedItemId, function(err) //dig into default List and find item by id, taken from checkbox value="<%=item._id%>"
   {
     if (err) {
       console.log(err);
     } else
     {
       console.log("Item removed");
-      res.redirect("/");
+      res.redirect("/"); //after item is deleted redirect to main
     }
   }
     );
 } else {
-  List.findOneAndUpdate(
-    {name: listName},
-    {$pull: {items: {_id:checkedItemId}}},
+  //if List is not default
+  List.findOneAndUpdate(   //mongoose specific function->  find Item and remove
+    {name: listName}, //conditions for filter -> looking for List with <%= listTitle %> as a name
+    {$pull: {items: {_id:checkedItemId}}}, //$pull updates(remove) item with specified id -> taken from checkbox value value="<%=item._id%>"
     function (err, foundList){
       if (!err) {
-        res.redirect("/" + listName);
+        res.redirect("/" + listName); //after item is removed redirect to updated list page
       }
     }
   );
@@ -117,39 +141,41 @@ if (listName === "Today")
 
 });
 
+/*Custom List page*/
 app.get("/:customListName", function (req,res)
 {
-  const customListName = _.capitalize(req.params.customListName);
+  const customListName = _.capitalize(req.params.customListName); //Get listname from custom link page name
 
-  List.findOne({name:customListName}, function(err, foundList)
+  List.findOne({name:customListName}, function(err, foundList)  //Find Custom List, if not found create new list
   {
     if (!err){
 
-      if (!foundList)
+      if (!foundList) //if Custom List does not exist already
       {
-
+        //Create List, fill with default items
         const list = new List(
           {
           name: customListName,
           items: defaultItems
           });
-          list.save();
-          res.redirect("/" + customListName);
+          list.save(); //save list to DB
+          res.redirect("/" + customListName); //redirect to custom list page
 
       }
       else
       {
-        res.render("list", {listTitle:foundList.name, newListItems:foundList.items});
+        res.render("list", {listTitle:foundList.name, newListItems:foundList.items}); //if List does exist -> redirect to list page passing Listname a a Title and fill with Items from DB
       }
     }
   });
 
 });
-
+/*About Section was created as express route testing scenario*/
 app.get("/about", function(req,res){
   res.render("about");
 });
 
+/*app runs on localhost:3000*/
   app.listen(3000, function(){
     console.log("Server is running on port 3000");
   });
